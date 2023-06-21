d4d43bece13ae22257f3e395f3520930fd5af5e9
pickles135 <tcho@kqed.org>
2023-06-20 17:15:59 -0700

    feat(KSM-4696, ./lib/FaIcon): configure imported stories in FaIcon as testing grounds for correct webpack + storybook implementation

---
+++ lib/FaIcon/FaIcon.stories.jsx
@@ -0,0 +1,10 @@
+//placeholder for testing grounds with Storybook implementation
+import { Meta } from '@storybook/react'
+import FaIcon from './FaIcon'
+
+export default {
+  title: 'Components/FaIcon/Font Awesome Icons',
+  component: FaIcon
+}
+
+export const AngleUpSolid = () => <FaIcon icon={'angleUpSolid'} />
