91e818fd54725dc489e7fbb5fcbcf10110d2eb32
pickles135 <tcho@kqed.org>
2023-06-22 16:11:31 -0700

    feat(KSM-4723, ./storybook/preview.js): import global styles from youthmedia and dot-org

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 /** @type { import('@storybook/react').Preview } */
+// kqed-main-site stylesheets
+import 'globalStyles/kqed-main-site/_base.scss'
+import 'globalStyles/kqed-main-site/core.scss'
+import 'globalStyles/kqed-main-site/_fonts.scss'
+import 'globalStyles/kqed-main-site/_mixins.scss'
+import 'globalStyles/kqed-main-site/_fontsV2.scss'
+
+// youth-media stylesheet
+import 'globalStyles/youth-media/_index.scss'
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
